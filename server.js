b95c3f4310fa60dbe10ad08250121ec9f4b2e45f
Cole Kepford <cole.kepford@uleth.ca>
2023-02-23 14:47:09 -0700

    set up authController and routes

--- server.js
+++ server.js
@@ -3,20 +3,31 @@ const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
 
+//db and authenticateUser
 import connectDB from './db/connect.js'
+
+// routers
+import authRouter from './routes/authRoutes.js'
 
 //middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 
+// Returns middleware that all parse json
+app.use(express.json())
 
+// Home address
 app.get('/', (req, res) => {
   res.send('Welcome!')
 })
 
+// authorization route
+app.use('/api/v1/auth', authRouter)
+
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
+// Use port from .env file or set port to 5000
 const port = process.env.PORT || 5000
 
 const start = async () => {
